a01f0b448ef4372c74c641f34f7e1ae6c6dc6166
Christy Perozzi <christy.perozzi@gmail.com>
2021-11-16 20:34:33 -0500

    commit tree 3cda8b508eec0d3884f7bc0d878ac58213a14af7

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+class Polygon {
+    constructor(array){
+        this.sidesArray = array;
+    }
+    get countSides(){
+        return this.sidesArray.length; 
+    }
+    get perimeter(){
+        return this.sidesArray.reduce((x,y) => x+y)
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        return (this.sidesArray[0] + this.sidesArray[1] > this.sidesArray[2] && this.sidesArray[0] + this.sidesArray[2] > this.sidesArray[1] && this.sidesArray[2] + this.sidesArray[1] > this.sidesArray[0]);
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        return (this.sidesArray[0] == this.sidesArray[1] && this.sidesArray[1] == this.sidesArray[2] && this.sidesArray[2] == this.sidesArray[3]  )
+    }
+
+    get area(){
+        return this.sidesArray[0] * this.sidesArray[1];
+    }
+}
+
+let myShape = new Polygon([10,10,10,10]); 
+let myTriangle = new Triangle([5,5,7]);
+let mySquare = new Square([2, 2, 2, 2])
+
+//console.log(myShape.perimeter);
+//console.log(myTriangle.isValid);
+console.log(mySquare.isValid)
